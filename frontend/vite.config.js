63ceb8cf5adebe7bc8646d11bddc1e6660c32210
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2025-07-19 00:02:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://directory-project-kappa.vercel.app/',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // Load env variables based on the mode (development/production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY,
+          changeOrigin: true
+        }
       }
     }
-  }
-}); 
+  };
+});
